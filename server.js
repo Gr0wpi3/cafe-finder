88b1cf1558359e2d2f2dc7558be05f7a9351090d
Gio <giovanni.work17@gmail.com>
2026-08-27 14:25:46 +0800

    fix: serve frontend through express

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
+app.use(express.static(path.join(__dirname)));
 
 const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.get("/api/test", (req, res) => {
     res.json({
